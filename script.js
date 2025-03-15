86359732ced49e3a8e33ee45b1dc5a5d1952eed8
sekendar1823 <sekendar1390>
2025-03-15 20:51:27 +0600

    html css assignment2 done

---
+++ script.js
@@ -0,0 +1,19 @@
+// let slides = document.querySelectorAll('.slide');
+// let dots = document.querySelectorAll('.dot');
+// let currentSlideIndex = 0;
+
+// function currentSlide(index) {
+//   slides[currentSlideIndex].classList.remove('active');
+//   dots[currentSlideIndex].classList.remove('active');
+
+//   currentSlideIndex = index;
+
+//   slides[currentSlideIndex].classList.add('active');
+//   dots[currentSlideIndex].classList.add('active');
+// }
+
+// // Auto Slide every 5 seconds
+// setInterval(() => {
+//   let nextIndex = (currentSlideIndex + 1) % slides.length;
+//   currentSlide(nextIndex);
+// }, 5000);
